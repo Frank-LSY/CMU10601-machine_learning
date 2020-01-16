48be94e24088d940929d56593e0a0071992521d6
Frank-LSY <429598469@qq.com>
2020-01-16 16:32:28 -0500

    hw1 01-16

--- hw1/decisionStump.py
+++ hw1/decisionStump.py
@@ -1,29 +1,32 @@
 import numpy as np
 import sys
 import random as rd
-import csv
 
-
-class LoadData:
-    def __init__(self, infile, outfile, attribute):
+# dealing with data
+class LoadData():
+    def __init__(self, infile, attribute):
         self.infile = infile
-        self.outfile = outfile
         self.attribute = attribute
         return
 
+    # load dataset from files
     def load_data(self):
-        with open(self.infile,'r') as f:
-            datafile = csv.reader(f,delimiter='\t')
-            dataset = []
-            attrCol = []
-            for index,row in enumerate(datafile):
-                if index==0:
-                    continue
-                dataset.append(row)
-                attrCol.append(row[self.attribute])
-        return dataset, attrCol
+        dataset = np.loadtxt(self.infile,dtype=str,delimiter='\t',skiprows=1)
+        return dataset
 
-    def get_value(self,dataset,colIndex):
+    # get specific column from the dataset
+    def get_col(self,colIndex):
+        dataset = self.load_data()
+        # print(colIndex)
+        col = []
+        for row in dataset:
+            # print(row[colIndex])
+            col.append(row[colIndex])
+        return col
+
+    #get values of specific column (binary)
+    def get_value(self,colIndex):
+        dataset = self.load_data()
         label1 = dataset[0][colIndex]
         for item in dataset:
             if item[colIndex]!= label1:
@@ -32,19 +35,20 @@ class LoadData:
         return label1, label2
 
 
+#Decision Stump Logic
+class DecisionStump():
+    def __init__(self,infile,outfile,attribute):
+        self.ld = LoadData(infile,attribute)
+        self.attribute = attribute
+        self.outfile = outfile
+        self.dataset = self.ld.load_data()
+        self.attrCol = self.ld.get_col(self.attribute)
 
-class DecisionStump:
-
-    def __init__(self,dataset,attr_col):
-        self.dataset = dataset
-        self.attrCol = attr_col
-        self.ld = LoadData()
-
-
-    def train(self,attribute):
-        dataset = LoadData.load_data(DecisionStump)[0]
-        attrCol = LoadData.load_data(DecisionStump)[1]
-        label = LoadData.get_value(dataset,attribute)
+    # train
+    def train(self):
+        dataset = self.dataset
+        attrCol = self.attrCol
+        label = self.ld.get_value(self.attribute)
         data0 = []
         data1 = []
         for index,row in enumerate(dataset):
@@ -58,10 +62,10 @@ class DecisionStump:
 
         return vote0, vote1
 
-
+    # majority vote
     def majority_vote(self,dataset):
         count1 = 0
-        label = self.get_value(dataset,-1)
+        label = self.ld.get_value(-1)
         for row in dataset:
             if row[-1] == label[0]:
                 count1 += 1
@@ -76,31 +80,63 @@ class DecisionStump:
         else:
             return rd.choice(label[0],label[1])
 
-    def h(self,dataset,attribute):
+    # predict
+    def h(self, vote_result):
+        predict_label = []
+        label = self.ld.get_value(self.attribute)
+        for row in self.dataset:
+            if row[self.attribute] == label[0]:
+                predict_label.append(vote_result[0])
+            else:
+                predict_label.append(vote_result[1])
+        predict_label = np.array(predict_label)
+        np.savetxt(self.outfile,predict_label,fmt='%s',delimiter='\n')
+        return predict_label
+
+    # evaluate
+    def evaluate(self):
+        mr = Metrics(self.ld.get_col(-1),self.h(self.train()))
+        return mr.error_rate()
+
+class Metrics():
+    def __init__(self,realcol,predcol):
+        self.realcol = realcol
+        self.predcol = predcol
         return
 
-
-class Metrics:
-    def __init__(self):
-        return
-
-    def error_rate(self,realcol, predcol):
+    def error_rate(self):
         count = 0
-        for i in range(len(realcol)):
-            if realcol[i] != predcol[i]:
+        for i in range(len(self.realcol)):
+            if self.realcol[i] != self.predcol[i]:
                 count += 1
 
-        er = count/len(realcol)
+        er = count/len(self.realcol)
         return er
 
 if __name__ == '__main__':
-    # infile = sys.argv[1]
-    # outfile = sys.argv[2]
-    # print("The input file is: %s" % (infile))
-    # print("The output file is: %s" % (outfile))
-    ds = DecisionStump('./handout/small_train.tsv','./tmp',0)
-    data = ds.load_data()
-    label = ds.get_value(data[0],0)
-    maj = ds.majority_vote(data[0])
-    tr = ds.train(0)
-    print(tr)
+    train_infile = sys.argv[1]
+    test_infile = sys.argv[2]
+    attr = sys.argv[3]
+    train_outfile = sys.argv[4]
+    test_outfile = sys.argv[5]
+    metrics_out = sys.argv[6]
+    # print("train_in:{}".format(train_infile))
+    # print("test_in:{}".format(test_infile))
+    # print("attr:{}".format(attr))
+    # print("train_out:{}".format(train_outfile))
+    # print("test_out:{}".format(test_outfile))
+    # print("metrics:{}".format(metrics_out))
+
+    train_ds = DecisionStump(train_infile,train_outfile,int(attr))
+    train_train = train_ds.train()
+    train_predict = train_ds.h(train_train)
+    train_errorrate = train_ds.evaluate()
+
+    test_ds = DecisionStump(test_infile,test_outfile,int(attr))
+    test_train = test_ds.train()
+    test_predict = test_ds.h(test_train)
+    test_errorrate = test_ds.evaluate()
+
+    with open(metrics_out,'w') as f:
+        f.writelines("error(train): {}\n".format(train_errorrate))
+        f.writelines("error(test): {}".format(test_errorrate))
--- hw1/test.py
+++ hw1/test.py
@@ -1,11 +1,12 @@
 import numpy as np
 import csv
 
-with open ('./handout/small_train.tsv','r') as f:
 
-    a = csv.reader(f,delimiter='\t')
 
-    for index,row in enumerate(a):
-        if index==0:
-            continue
-        print(row[0])
+a= np.loadtxt('./handout/small_train.tsv', delimiter='\t',dtype=str,skiprows=1)
+for index,rows in enumerate(a):
+    print(rows)
+    print(index)
+# print(a)
+
+# with open ('./handout/small_train.tsv','r') as f:
